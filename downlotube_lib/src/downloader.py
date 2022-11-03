56b60cca0d17b1fdccc302911ded1a90432b4990
Raxabi <beltrandiaztorres@gmail.com>
2022-11-03 23:58:15 +0100

    [ADD] Added line break at the final message

--- downlotube_lib/src/downloader.py
+++ downlotube_lib/src/downloader.py
@@ -13,5 +13,5 @@ def downloader(input_url: str, itag_selector: str, download_path: str):
     if os.path.isfile(download_path):
         print("Error, la ruta donde se vaya a guardar la descarga debe de ser una carpeta")
     elif os.path.isdir(download_path):
-        print(f"La carpeta '{download_path}' es una carpeta valida, estamos trabajando en la descarga...\nMuchas gracias por usar downlotube")
+        print(f"La carpeta '{download_path}' es una carpeta valida, estamos trabajando en la descarga...\n\nMuchas gracias por usar downlotube")
         url.streams.get_by_itag(itag_selector).download(download_path)
